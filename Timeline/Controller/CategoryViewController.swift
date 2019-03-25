39ff58b1294c63c9b10dbc3ad6c974bcaab69375
Erik Salas <25646533+eriksalas@users.noreply.github.com>
2019-03-25 17:21:39 -0500

    persisted data using Core Data

---
+++ Timeline/Controller/CategoryViewController.swift
@@ -0,0 +1,177 @@
+//
+//  CategoryViewController.swift
+//  Timeline
+//
+//  Created by Erik Salas on 3/24/19.
+//  Copyright © 2019 Erik Salas. All rights reserved.
+//
+
+import UIKit
+import CoreData
+
+class CategoryViewController: UITableViewController {
+    //use the app delegate to reach the persistentContainer viewContext
+    let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+    //create an array of Category objects
+    var categories = [Category]()
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+        
+        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+        loadCategories()
+    }
+    
+    // MARK: - Table View Datasource Methods
+    
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 1
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        //return the number of objects in the categories array
+        return categories.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        //create a reusable cell
+        let cell = tableView.dequeueReusableCell(withIdentifier: "CategoryCell", for: indexPath)
+        //create a reference to the category in the current categories[indexPath.row]
+        let category = categories[indexPath.row]
+        //set the text label to the name of the category at the current index path
+        cell.textLabel?.text = category.name
+        //return the custom cell to be displayed
+        return cell
+    }
+    
+    /*
+     // Override to support conditional editing of the table view.
+     override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+     // Return false if you do not want the specified item to be editable.
+     return true
+     }
+     */
+    
+    /*
+     // Override to support editing the table view.
+     override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+     if editingStyle == .delete {
+     // Delete the row from the data source
+     tableView.deleteRows(at: [indexPath], with: .fade)
+     } else if editingStyle == .insert {
+     // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+     }
+     }
+     */
+    
+    /*
+     // Override to support rearranging the table view.
+     override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
+     
+     }
+     */
+    
+    /*
+     // Override to support conditional rearranging of the table view.
+     override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
+     // Return false if you do not want the item to be re-orderable.
+     return true
+     }
+     */
+    
+    // MARK: - Table View Delegate Methods
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        //trigger the segue that will take you to the list of items associated with the current selected category
+        performSegue(withIdentifier: "goToItems", sender: self)
+    }
+    
+    //prepare before triggering a new seque
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        //set the destination view controller as the assumed view controller
+        let destinationVC = segue.destination as! ItemViewController
+        //access the selectedCategory property of the destination view controller and set it to the
+        //current selected category to load the items associated with it
+        if let indexPath = tableView.indexPathForSelectedRow {
+            destinationVC.selectedCategory = categories[indexPath.row]
+        }
+    }
+
+    
+    
+    // MARK: - Add New Categories
+    @IBAction func addCategoryButton(_ sender: UIBarButtonItem) {
+        //create a local variable that will hold the name of the new category once typed in by the user
+        //in the alert's text field
+        var textField = UITextField()
+        //create an alert to allow user to enter a new category
+        let alert = UIAlertController(title: "Add New Category", message: "", preferredStyle: .alert)
+        //create an action button that the user can touch once they are ready to add a new category
+        let action = UIAlertAction(title: "Add Category", style: .default) { (action) in
+            //create a new category reference in the context
+            let newCategory = Category(context: self.context)
+            //set the newCategory name from the textField's input
+            newCategory.name = textField.text!
+            //append the new category to the category array
+            self.categories.append(newCategory)
+            //save the new category into the database
+            self.saveCategories()
+            
+        }
+        //add a textField that the users can use to add the new category
+        alert.addTextField { (alertTextField) in
+            //set a textfield placeholder
+            alertTextField.placeholder = "Add new category"
+            //set the local textfield variable equal to the one typed into the textfield in the closure
+            textField = alertTextField
+        }
+        
+        //add the action to the alert
+        alert.addAction(action)
+        
+        //present the alert to the user once they click on add new category button
+        present(alert, animated: true, completion: nil)
+        
+    }
+    
+    // MARK: - Data Manipulation Methods
+    //save a new category into the database
+    func saveCategories() {
+        do {
+            try context.save()
+        } catch {
+            print(error)
+        }
+        //reload the tableview
+        tableView.reloadData()
+    }
+    
+    //load all categories saved in the database
+    func loadCategories(with request: NSFetchRequest<Category> = Category.fetchRequest()) {
+        do {
+            //assign all the Category objects saved in the database into the local categories array
+            categories = try context.fetch(request)
+        } catch {
+            print(error)
+        }
+        //reload the table view data shown to the user
+        tableView.reloadData()
+    }
+    
+    
+    /*
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destination.
+     // Pass the selected object to the new view controller.
+     }
+     */
+}
--- Timeline/Controller/ItemViewController.swift
+++ Timeline/Controller/ItemViewController.swift
@@ -7,81 +7,181 @@
 //
 
 import UIKit
+import CoreData
 
 class ItemViewController: UITableViewController {
-    
-    let defaults = UserDefaults.standard
-    
-    var itemArray = [Item()]
+    //use the app delegate to reach the persistentContainer viewContext
+    let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+
+    //create an array of Item() objects
+    var items = [Item]()
+    var selectedCategory: Category? {
+        didSet {
+            loadItems()
+        }
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
-        let newItem = Item()
-        newItem.title = "Erik"
-        itemArray.append(newItem)
-        
-//        if let items = defaults.array(forKey: "ItemArray") as? [String] {
-//            itemArray = items
-//        }
+        //load the items in the lists when the app loads
     }
     
     // MARK: - Tableview datasource methods
     
+    //specify the number of sections that will be in the table
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return itemArray.count
+        return items.count
     }
     
+    //send the data from the itemArray to be displayed in the table's cells
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        //create the reusable cell that will be used to display all the information on the table
         let cell = tableView.dequeueReusableCell(withIdentifier: "ItemCell", for: indexPath)
         
-        let item = itemArray[indexPath.row]
-        
+        //create a reference to the item in the itemArray at the current indexPath
+        let item = items[indexPath.row]
+        //set the cell's label to the tile of the itemArray item at the current indexPath
         cell.textLabel?.text = item.title
-        
+        //set the accessoryType of the item in the itemArray at the current indexPath
+        //depending on the state of the item.done property
         cell.accessoryType = item.done ? .checkmark : .none
         
+        //return the custom cell at the current itemArray indexPath.row
         return cell
     }
     
     // MARK: - Tableview delegate methods
     
+    //determine when a cell is selected by the user
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         
-        itemArray[indexPath.row].done = !itemArray[indexPath.row].done
+        //if the cell is tapped by the user, set the item.done property to the opposite of
+        //what it currently is
+        items[indexPath.row].done = !items[indexPath.row].done
         
-        tableView.reloadData()
+        //call the saveItems method that will reach into the context to save the updated table
+        //data into the persistantContainer
+        saveItems()
         
+        //use the tableView.deslectRow to remove the gray background from a cell when it is selected
         tableView.deselectRow(at: indexPath, animated: true )
     }
 
     //MARK: - Add new items function
     
+    //Create an action for the add item (+) bar button that will add a new item into the itemArray
     @IBAction func addItemButton(_ sender: UIBarButtonItem) {
+        //create a local UITextField variable that will hold the new item to be saved into the context and then persistantContainer
         var textField = UITextField()
        
+        //create a new aleart to allow the user to add an new item to their list
         let alert = UIAlertController(title: "Add New Item", message: "", preferredStyle: .alert)
         
+        //create an action button to the alert that will allow the user to confirm to add a new item
         let action = UIAlertAction(title: "Add Item", style: .default) { (action) in
             
-            let newItem = Item()
+            //inside this closure, a new item will be created with all of its properities, added to the context, and appended
+            //to the local itemArray
+            let newItem = Item(context: self.context)
             newItem.title = textField.text!
-            self.itemArray.append(newItem)
-//            self.defaults.set(self.itemArray, forKey: "ItemArray")
-            self.tableView.reloadData()
+            newItem.done = false
+            newItem.parentCategory = self.selectedCategory
+            self.items.append(newItem)
+            
+            //then the saveItems method will access context to save updated itemArray into the
+            //persistent container
+            self.saveItems()
         }
         
+        //add a texfield to the alert that will temporaraly hold the new item typed by the user and then saved into the
+        //local textField variable
         alert.addTextField { (alertTextField) in
             alertTextField.placeholder = "Create new item"
             textField = alertTextField
         }
         
+        //add the action to the alert
         alert.addAction(action)
         
+        //present the alert to the user
         present(alert, animated: true, completion: nil)
     }
-  
+    
+    //MARK: - Model manipulation methods
+    
+    //the function that will be called every time the table is modified
+    func saveItems() {
+        //throws, so check if context is able to save into the persistantContainer
+        do {
+            try context.save()
+        } catch {
+            print(error)
+        }
+        
+        //reload the table to show updated data
+        tableView.reloadData()
+    }
 
+    //the function that will be used to load items from the persistantContainer into the tableview
+    //if there is no initial request or predicate, then there will be a defualt request and predicate will be nil
+    //if there is a specified request and predicate passed into the loadItems func, the specified
+    //request/predicate will be used
+    func loadItems(with request: NSFetchRequest<Item> = Item.fetchRequest(), predicate: NSPredicate? = nil) {
+        //predicate that will return only items that are owned by specified category
+        let categoryPredicate = NSPredicate(format: "parentCategory.name MATCHES %@", selectedCategory!.name!)
+        
+        //if a predicate is provided into the loadItems func, then both the additional predicate and the categoryPredicate
+        //will be used by the request
+        if let additionalPredicate = predicate {
+            request.predicate = NSCompoundPredicate(andPredicateWithSubpredicates: [categoryPredicate, additionalPredicate])
+        } else {
+        //if no default predicate is provided into the loadItems func, then only the categoryPredicate will be used by request
+            request.predicate = categoryPredicate
+        }
+        
+        //fetch the items specified by the request and predicate into the local items array
+        do {
+            items = try context.fetch(request)
+        } catch {
+            print(error)
+        }
+        
+        //reload the table to show updated data
+        tableView.reloadData()
+    }
+}
+
+//MARK: - Search bar methods
+
+// an extension that will be using the UISearchBarDelegate protocol and will be using some of its
+//delegate methods
+extension ItemViewController: UISearchBarDelegate {
+    //call a delegate method that will detect when the search bar is clicked
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        //create a request to be sent to the persistantContainer
+        let request: NSFetchRequest<Item> = Item.fetchRequest()
+        
+        //create a predicate that will determine the request's search along with a way to sort the
+        //results returned
+        let predicate = NSPredicate(format: "title CONTAINS[cd] %@", searchBar.text!)
+        request.sortDescriptors = [NSSortDescriptor(key: "title", ascending: true)]
+        
+        //call the loadItems method with the new search request, predicate, and sort method
+        loadItems(with: request, predicate: predicate)
+    }
+    
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        if searchBar.text?.count == 0 {
+            //show all items when the searchBar.text count is reset to 0
+            loadItems()
+            
+            //disable the search bar after the x button is pressed to clear it, setting it to be done by the main thread
+            DispatchQueue.main.async {
+                searchBar.resignFirstResponder()
+            }
+        }
+    }
 }
 
